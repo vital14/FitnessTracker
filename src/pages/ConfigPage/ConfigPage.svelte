e29d312a1d1aa841792a58dff6e80b72d9477ce0
vital14 <limavictor49@gmail.com>
2023-09-04 20:41:41 -0300

    finalizando configurações

--- src/pages/ConfigPage/ConfigPage.svelte
+++ src/pages/ConfigPage/ConfigPage.svelte
@@ -1,20 +1,31 @@
 <script>
     import HeaderConfig from "./components/HeaderConfig.svelte";
     import Timer from "./components/Timer.svelte";
+    import NewObjective from "./components/NewObjective.svelte";
 </script>
 
 <HeaderConfig></HeaderConfig>
-<div class="label">Horário de atividade:</div>
+<div class="time">Horário de atividade:</div>
 <Timer></Timer>
+<div class="objectives">Objetivos:</div>
+<NewObjective></NewObjective>
 
 
 <style>
-    .label{
+    .time{
         margin-left: 5vw;
+        margin-top: 2vh;
         height: 5vh;
         width: 40vw;
         background-color: transparent;
         text-align: center;
         justify-content: center;
+    }
+    .objectives{
+        margin-left: 5vw;
+        margin-top: 2vh;
+        height: 5vh;
+        width: 40vw;
+        background-color: transparent;
     }
 </style>
---
+++ src/pages/ConfigPage/components/NewObjective.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	import waterBottle from '$assets/waterBottle.svg';
+	import breakImage from '$assets/fitness.svg';
+
+	import { waterObjective, breakObjective } from '$stores/store';
+
+	let _breakObjective: string;
+	let _waterObjective: string;
+
+	breakObjective.subscribe((value) => (_breakObjective = value.toString()));
+	waterObjective.subscribe((value) => (_waterObjective = value.toString()));
+
+	function updateBreaks() {
+		const input = document.getElementById('breakObjective') as HTMLInputElement | null;
+		const inputContet = input?.value;
+		if (inputContet != undefined) {
+			let newValue: number = parseInt(inputContet);
+			breakObjective.set(newValue);
+		}
+	}
+
+	function updateWaterObjective() {
+		const input = document.getElementById('waterObjective') as HTMLInputElement | null;
+		const inputContet = input?.value;
+		if (inputContet != undefined) {
+			let newValue: number = parseFloat(inputContet);
+			waterObjective.set(newValue);
+		}
+	}
+</script>
+
+
+		<div class="wrapper-water">
+			<img class="objective-water" src={waterBottle} alt="Water" />
+			<div class="objective-data">
+				<input
+					type="number"
+					class="custom"
+					id="waterObjective"
+					inputmode="decimal"
+					value={_waterObjective}
+					on:change={() => updateWaterObjective()}
+				/>
+			</div>
+		</div>
+
+        <div class="wrapper-break">
+			<img class="objective-break" src={breakImage} alt="Water" />
+			<div class="objective-data">
+				<input
+					type="number"
+					class="custom-break"
+					id="waterObjective"
+					inputmode="decimal"
+					value={_breakObjective}
+					on:change={() => updateBreaks()}
+				/>
+			</div>
+		</div>
+
+<style>
+.objective-water{
+    width: 30px;
+    height: 30px;
+    margin-left: 3vw;
+}
+.objective-break{
+    width: 30px;
+    height: 30px;
+    margin-left: 1vw;
+}
+.wrapper-water {
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    padding: 15px 20px;
+    display: flex;
+    background-color: rgb(137, 207, 240) ;
+    width: 55vw;
+    align-items: center;
+    justify-content: space-between;
+    margin-left: 5vw;
+    margin-top: 2vw;
+  }
+  .wrapper-break {
+    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    padding: 15px 20px;
+    display: flex;
+    background-color: #F98617;
+    width: 55vw;
+    align-items: center;
+    justify-content: space-between;
+    margin-left: 5vw;
+    margin-top: 2vw;
+  }
+  .custom{
+	background-color: rgb(137, 207, 240) ;
+	border: none;
+	text-align: center;
+    margin-left: 2vw;
+}
+.custom-break{
+	background-color: #F98617;
+	border: none;
+	text-align: center;
+    margin-left: 1vw;
+}
+</style>
